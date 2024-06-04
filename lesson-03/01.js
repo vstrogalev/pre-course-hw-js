9af45a3151fcf1f0655e0749b00a71271169553c
Viacheslav Strogalev <strogalev@hotmail.com>
2024-06-04 19:38:41 +0300

    lesson 03 task 01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((el) => el === "черный" || el === "красный" || el === "желтый").join('-')
+}
+
+console.log(createColorString());
+
